6ff4e1b5f98ba22d0b78e769f491a9718dbf70ab
Lennox Mountain <lennoxmountain67@gmail.com>
2024-07-02 21:48:26 +0200

    designing barelyHR homepage first section

--- barely-hr/app/page.tsx
+++ barely-hr/app/page.tsx
@@ -1,7 +1,29 @@
 export default function Home() {
   return (
-    <div className="max-w-2xl mx-auto">
-      <h1>Hello world</h1>
-    </div>
+    <main className="">
+      <div className="px-2 pt-2">
+        <div className="bg-fuchsia-200 rounded-[2rem] pt-16 pb-48 px-24" >
+          <nav>
+            <div>
+              <span>Logo</span>
+              <span>Banner</span>
+            </div>
+            <div>
+              <a href="#">Features</a>
+              <a href="#">Pricing</a>
+              <a href="#">Login</a>
+            </div>
+          </nav>
+          <div>
+            <h1>Hire Smarter.</h1>
+            <p>BarelyHR helps small and large companies find the right person  for the right job.</p>
+            <div>
+              <a href="#">Get started</a>
+              <a href="#">Learn more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
