3f402561df7696bcfb82cca4c0d2c374e31a44ef
Lennox Mountain <lennoxmountain67@gmail.com>
2024-07-03 10:18:32 +0200

    added background color, installed tailwindcss v4, restyled homepage layout and styled the buttons

--- barely-hr/app/page.tsx
+++ barely-hr/app/page.tsx
@@ -2,24 +2,24 @@ export default function Home() {
   return (
     <main className="">
       <div className="px-2 pt-2">
-        <div className="bg-fuchsia-200 rounded-[2rem] pt-16 pb-48 px-24" >
-          <nav>
-            <div>
-              <span>Logo</span>
-              <span>Banner</span>
+        <div className="bg-gradient-to-br from-[#FFF1BE] via-[#EEE87C] to-[#b060ff] rounded-[2rem] pt-16 pb-48 px-24" >
+          <nav className="flex items-center justify-between py-3">
+            <div className="flex items-center gap-6">
+              <a href=""><span className="font-bold text-xl">🧣BarelyHR</span></a>
+              <div className="rounded-full bg-fuchsia-950/35 text-sm/6 font-medium text-white px-3 py-0.5">BarelyHR raises $100M Series of A from Tailwind Ventures</div>
             </div>
-            <div>
-              <a href="#">Features</a>
-              <a href="#">Pricing</a>
-              <a href="#">Login</a>
+            <div className="flex gap-8">
+              <a className="text-base font-medium text-gray-950 " href="#">Features</a>
+              <a className="text-base font-medium text-gray-950 " href="#">Pricing</a>
+              <a className="text-base font-medium text-gray-950 " href="#">Login</a>
             </div>
           </nav>
-          <div>
-            <h1>Hire Smarter.</h1>
-            <p>BarelyHR helps small and large companies find the right person  for the right job.</p>
-            <div>
-              <a href="#">Get started</a>
-              <a href="#">Learn more</a>
+          <div className="mt-40">
+            <h1 className="text-9xl font-medium text-gray-950">Hire Smarter.</h1>
+            <p className="text-xl/8 font-medium text-gray-950/75">BarelyHR helps small and large companies find the right person  for the right job.</p>
+            <div className="mt-12 flex gap-6 items-center">
+              <a className="py-3 px-4 rounded-full bg-gray-950 text-white  text-base/4 shadow-md" href="#">Get started</a>
+              <a className="py-3 px-4 rounded-full bg-white/15 text-gray-950  text-base/4 shadow-md inset-shadow[0_0_2px_1px_#fffff4d] ring-[#D15052]/15"  href="#">Learn more</a>
             </div>
           </div>
         </div>
